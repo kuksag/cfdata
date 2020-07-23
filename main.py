ec09700e03a48dba3859265f27aa3d9245988660
kuksag <georgiy.kuksa@yandex.ru>
2020-07-23 16:54:48 +0500

    теперь main.py рисует графики

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 import requests
 import time
 
@@ -24,6 +25,49 @@ def ask_profile():
             print(response['comment'])
     response = response['result'][0]
     return response
+
+
+def draw_pie(languages, name, legend):
+    languages = [[k, v] for k, v in languages.items()]
+    languages.sort(key=lambda i: -i[1])
+    print(languages)
+    labels = [item[0] for item in languages]
+    sizes = [item[1] for item in languages]
+    explode = [0 for i in range(len(labels))]
+    if len(explode) > 2:
+        explode[1] = 0.1
+
+    fig, ax = plt.subplots()
+
+    def my_pct(i):
+        if i > 5:
+            return str(int(i * 10) / 10) + "%"
+
+    def my_label(i):
+        if 100 * sizes[i] / sum(sizes) > 5:
+            return labels[i]
+        else:
+            return ""
+
+    wedges, text, autotexts = ax.pie(sizes, explode=explode,
+                                     labels=[my_label(i) for i in range(len(labels))],
+                                     autopct=my_pct)
+
+    for i in range(len(labels)):
+        labels[i] += " (" + str(sizes[i]) + ")"
+
+    if len(labels) > 5:
+        labels = labels[0:5]
+
+    ax.legend(wedges, labels,
+              title = legend,
+              bbox_to_anchor = (0.1, 0.1))
+
+    plt.setp(autotexts, size=10, weight="bold")
+
+    ax.set_title(name)
+
+    plt.show()
 
 
 def ask_submits():
@@ -68,8 +112,12 @@ def ask_submits():
 
     print("number of attempted tasks:", len(attempted))
     print("number of solved tasks:", len(solved))
-    for item in tags:
-        print("{}:".format(item), tags[item])
+
+    print(tags)
+
+    draw_pie(languages, "Language of submits statistics", "Programming languages")
+    draw_pie(tags, "Tasks statistics", "Tags")
+    draw_pie(verdicts, "Verdicts statistics", "Verdicts")
 
     return response
 
@@ -88,5 +136,5 @@ with requests.Session() as cur_sess:
     api = "http://codeforces.com/api/"
     last_call = time.time()
     profile = ask_profile()
-    ask_submits()
+    submits = ask_submits()
     rate_diff = ask_rate_diff()
